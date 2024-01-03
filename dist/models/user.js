8acf4427f65187d9e4d0dc13a40b313be791dbce
Sami Khan <sami999khan999@gmail.com>
2024-01-03 23:52:26 +0600

    first commit

---
+++ dist/models/user.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import validator from "validator";
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    _id: {
+        type: String,
+        required: [true, "Please enter your ID"],
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter your Name"],
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already Exists"],
+        required: [true, "Please enter your Email"],
+        validate: validator.default.isEmail,
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add your Photo"],
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+        required: [true, "Please enter your Gender"],
+    },
+    dob: {
+        type: Date,
+        required: [true, "Please enter you Date of birth"],
+    },
+}, { timestamps: true });
+userSchema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+    if (today.getMonth() < dob.getMonth() ||
+        (today.getMonth() === dob.getMonth() && today.getDay() < dob.getDay())) {
+        age--;
+    }
+    return age;
+});
+export const User = mongoose.model("User", userSchema);
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import userRoute from "./routes/user.js";
+
+const port = 3000;
+
+const app = express();
+
+// user Router
+app.use("/api/v1/user", userRoute);
+
+app.listen(() => {
+  console.log(`Server is listening on port:${port}`);
+});
