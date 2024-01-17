c098f68332f0250a106f75c6226ff6073f56c20e
Sami Khan <sami999khan999@gmail.com>
2024-01-17 23:42:41 +0600

    done

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,7 @@ import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
 import Stripe from "stripe";
+import cors from "cors";
 // route path
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
@@ -24,6 +25,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 // Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
 import Stripe from "stripe";
+import cors from "cors";
 
 // route path
 import userRoute from "./routes/user.js";
@@ -31,6 +32,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // Routes
 app.use("/api/v1/user", userRoute);
